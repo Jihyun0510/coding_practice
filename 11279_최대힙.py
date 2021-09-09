5bfc5a87572eb76c74def19218c982da367e9e58
jihyun0510 <wgus5950@gmail.com>
2021-09-10 03:06:00 +0900

    input버전

---
+++ 11279_최대힙.py
@@ -0,0 +1,14 @@
+import heapq
+
+heap = []
+n = int(input())
+
+for _ in range(n):
+    x = int(input())
+    if x != 0:
+        heapq.heappush(heap, -x)
+    else:
+        try: 
+            print(-heapq.heappop(heap))
+        except:
+            print(0)
