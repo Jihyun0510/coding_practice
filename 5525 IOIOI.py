67384792ebcab4c6db0eedb5980d693086250d81
jihyun0510 <wgus5950@gmail.com>
2021-09-12 23:46:35 +0900

    delete n

--- 5525 IOIOI.py
+++ 5525 IOIOI.py
@@ -1,12 +1,12 @@
 import sys
 n = int(input())
-for _ in range(n):
-    # m = int(sys.stdin.readline().rstrip())
-    m = int(input())
-    # s = stdin.readline().rstrip()
-    s = input()
-    answer = 0
-    for i in range(1,m-1):
-        if s[i] == 'O' and  s[i-1] == 'I' and s[i+1] == 'I':
-            answer += 1
-    print(answer)
+# m = int(sys.stdin.readline().rstrip())
+m = int(input())
+# s = sys.stdin.readline().rstrip()
+s = input()
+answer = 0
+rep = 0
+for i in range(1,m-1):
+    if s[i] == 'O' and  s[i-1] == 'I' and s[i+1] == 'I':
+        answer += 1
+print(answer)
