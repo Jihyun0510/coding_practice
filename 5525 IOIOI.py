798912648fe8cd23d6cbf9ae1b1bdfd0cf6dde35
jihyun0510 <wgus5950@gmail.com>
2021-09-13 01:01:45 +0900

    n 반영

--- 5525 IOIOI.py
+++ 5525 IOIOI.py
@@ -8,5 +8,13 @@ answer = 0
 rep = 0
 for i in range(1,m-1):
     if s[i] == 'O' and  s[i-1] == 'I' and s[i+1] == 'I':
-        answer += 1
+        rep += 1
+        if rep == n:
+            answer += 1
+            rep -= 1
+    elif s[i] == 'I' and s[i-1] == 'O' and s[i+1] == 'O':
+        continue
+    else:
+        rep = 0
+
 print(answer)
