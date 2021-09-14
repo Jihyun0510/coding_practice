8a967de9efe18028c1b924cd7a707e6975a50075
jihyun0510 <wgus5950@gmail.com>
2021-09-14 16:48:39 +0900

    first trial

--- 1074 Z.py
+++ 1074 Z.py
@@ -1,7 +1,15 @@
 n, r, c = map(int, input().split())
 
-answer = 8*(r//2) + 4*(c//2)
-if r%2 == 1: answer += 2
-if c%2 == 1: answer += 1
+answer=0
+for i in range(n):
+    if i == 1:
+        if r%2 == 1: answer += 2
+        if c%2 == 1: answer += 1
+    else:
+        if r%(2**(i-1)) == 0:
+                answer += ((2**(2*i-1))
+        if c%(2**(i-1)) == 0:
+                answer += ((2**(2*i-2))
+    
 
 print(answer)
