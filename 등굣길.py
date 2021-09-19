560d547213a8654b0eb8bb66b1e812ec43ed05fa
jihyun0510 <wgus5950@gmail.com>
2021-09-19 22:25:03 +0900

    Co-authored-by: Yunsung Choi <choi-yunsung@users.noreply.github.com>

--- 등굣길.py
+++ 등굣길.py
@@ -7,10 +7,12 @@ def solution(m, n, puddles):
         for j in range(1,m+1):
             if i == 1 and j == 1:
                 dp[i][j] = 1
-            elif [i,j] in puddles:
+                continue
+            # 0 => False
+            if [i,j] in puddles:
                 dp[i][j] = False
                 continue
-            elif dp[i-1][j] != False and dp[i][j-1] != False: 
-                dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % 1000000007
-            
+            dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % 1000000007
+    
+    print(dp)
     return dp[n][m]
