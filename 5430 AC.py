bd29f50147a8431afb6e8d7cb8e5e6a33b73fb0e
jihyun0510 <wgus5950@gmail.com>
2021-09-29 21:00:18 +0900

    1st trial

---
+++ 5430 AC.py
@@ -0,0 +1,33 @@
+from collections import deque
+import sys
+
+t = int(input())
+
+for _ in range(t):
+    p = input()
+    reverse = False
+    n = int(input())
+    case = deque(input()[1:-1].split(','))
+
+    for op in p:
+        if op == 'R': 
+            if reverse:
+                reverse = False
+            elif not reverse:
+                reverse = True
+        else:
+            if not reverse and n:
+                case.popleft()
+                n -= 1
+            elif reverse and n:
+                case.pop()
+                n -= 1
+            else:
+                print('error')
+                break
+    else:
+        if not reverse:
+            print("["+",".join(case)+"]")
+        elif reverse:
+            case.reverse()
+            print("["+",".join(case)+"]")
