fcc2acbdcea952c90aefc20ace2cc169003a110d
jihyun0510 <wgus5950@gmail.com>
2021-10-05 00:46:11 +0900

    2nd trial (with describtion)

--- 7662 이중 우선순위 큐.py
+++ 7662 이중 우선순위 큐.py
@@ -1,31 +1,46 @@
+import sys;read=sys.stdin.readline
 import heapq
-import sys
-t = int(input())
-for _ in range(t):
-    q = []
-    # inv_q = []
 
-    k = int(input())
-    for _ in range(k):
-        a,b = input().split()
-        if a == 'I':
-            heapq.heappush(q, int(b))
-            # print(q)
-            # heapq.heappush(inv_q, -int(b))
-            # print(q)
-            # print(inv_q)
-
-        elif a == 'D':
-            try:
-                if int(b) >= 0:
-                    q.pop()
-                    # print(q)
-                elif int(b) < 0: 
-                    heapq.heappop(q)
-                    # print(q)
-            except:
-                continue
-    if len(q) > 0:
-        print(q[-1], q[0])    
-    else:
-        print('EMPTY')
+result=[]
+for T in range(int(read())):
+    # 각 step별 동기화 여부를 기록
+    visited=[False]*1_000_001
+    # 최소힙, 최대힙 생성
+    minH,maxH=[],[]
+    
+    for i in range(int(read())):
+        s=read().split()
+        # I 명령어 = 최소힙, 최대힙에 동시에 입력
+        # 해당 step은 동기화과 되어있는 것으로 표시 (True)
+        if s[0]=='I':
+            heapq.heappush(minH,(int(s[1]),i))
+            heapq.heappush(maxH,(-int(s[1]),i))
+            visited[i]=True
+        # D 1 명령어 = 최대힙만 업데이트
+        elif s[1]=='1':
+            # target인 최대값을 삭제하기 전,
+            # 이전에 최소힙에서는 삭제되었지만 아직 동기화되지 않은 값들 모조리 삭제
+            while maxH and not visited[maxH[0][1]]:heapq.heappop(maxH)
+            
+            # 삭제할 값이 없다면 실행되지 않고 이번 step은 넘어감
+            if maxH:
+                # 이번 step은 동기화되지 않았음을 기록
+                visited[maxH[0][1]]=False
+                # 원래 target이 맨 앞에 있으니 삭제
+                heapq.heappop(maxH)
+        else:
+            # target인 최소값을 삭제하기 전,
+            # 이전에 최대힙에서는 삭제되었지만 아직 동기화되지 않은 값들 모조리 삭제
+            while minH and not visited[minH[0][1]]:heapq.heappop(minH)
+            
+            # 삭제할 값이 없다면 실행되지 않고 이번 step은 넘어감
+            if minH:
+                # 이번 step은 동기화되지 않았음을 기록
+                visited[minH[0][1]]=False
+                # 원래 target이 맨 앞에 있으니 삭제
+                heapq.heappop(minH)
+    # 아직 동기화되지 않은 잔여 명령어들을 처리
+    while minH and not visited[minH[0][1]]:heapq.heappop(minH)
+    while maxH and not visited[maxH[0][1]]:heapq.heappop(maxH)
+    result.append(f'{-maxH[0][0]} {minH[0][0]}'if maxH and minH else'EMPTY')
+print('\n'.join(result))
