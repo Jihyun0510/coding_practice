8b40123f7db5a9566e1cea2e242015a51984fb0e
jihyun0510 <wgus5950@gmail.com>
2021-10-13 21:51:37 +0900

    2nd trial

--- 1107 리모컨.py
+++ 1107 리모컨.py
@@ -4,23 +4,25 @@ n= int(input())
 m = int(input())
 
 # malfuncs = [6,7,8]
-malfuncs = map(int, input().split())
+malfuncs = set(input().split())
 now = 100
 
-# +,- 사용할 시
-answer = abs(n - now)
+# 채널을 돌리는 방법 두가지
+# 1) +,- 만 사용
+answer = abs(now-n)
 
-# 채널 버튼을 누르고 +,-을 사용할 시
-for channel in range(100000):
+# 2) 채널 버튼을 누르고 +,-을 사용
+for channel in range(1000001):
     # 버튼으로 누를 수 있는 숫자인지 체크
     for num in list(str(channel)):
-        if int(num) in malfuncs:
+        if num in malfuncs:
             break
     # 다 누를 수 있다면, 해당 숫자에서 +,-을 사용할 시
     else:
-        # if abs(n-channel)+1 < answer:
+        # if abs(n-channel)+len(str(channel)) < answer:
         #     print(channel)
         answer = min(answer, abs(n-channel)+len(str(channel)))
+
         
 
 print(answer)
